459968fc448a5675c58e5680199d8314446f7919
Pablo-Tello <ptellot@uni.pe>
2023-05-26 15:57:36 -0500

    Perfil v1

--- src/app/store/perfil/perfil.component.ts
+++ src/app/store/perfil/perfil.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./perfil.component.scss']
 })
 export class PerfilComponent {
+  
+  nombre:string = "Enrique";
+  apellido:string = "Aguilar Torres";
+  correo:string = "enrique.torres@gmail.com";
+  dni:number = 32748229;
+  password:string = "****************";//"cliente_Pa55w0rd";
 
+  direccion:string = "Av. Los Virreyes #135 Coop. Viña San Francisco";
+  mz:string = "B";
+  lote:number = 14;
+  piso:number = 1;
+  referencia:string = "Al costado de la botica Inkafarma";
+  distrito:string = "Santa Anita";
+  celular:number = 998472663;
+
+  nameOwnerCard:string = "Enrique Aguilar Torres";
+  numCard:string = "4252****";
 }
