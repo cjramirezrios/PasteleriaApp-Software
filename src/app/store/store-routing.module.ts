8ae875a1447512336c9c39c5f555c10e9aa948f6
Pablo-Tello <ptellot@uni.pe>
2023-06-04 13:21:24 -0500

    reestructurando componentes sin repeticiones - parte 1

--- src/app/store/store-routing.module.ts
+++ src/app/store/store-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
         component:ProductoComponent
       },
       {
-        path:'detalle',
+        path:'productos/detalle',
         component:DetalleComponent
       },
       {
